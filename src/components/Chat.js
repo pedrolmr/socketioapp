2d3382a2242cd27e531d5e193b9cb1d77cad2b31
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-01 22:52:39 -0400

    created Chat and Home files

---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Chat extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Chat.js</Text>
+            </View>
+        );
+    }
+}
+export {Chat};
