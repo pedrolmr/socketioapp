1c80757f72475bf1fca0d17b835159fe7c396f85
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-01 22:56:49 -0400

    installed react-native-router-flux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Home, Chat } from './components'
+
+import { Router, Scene } from 'react-native-router-flux';
 
 export default function App() {
   return (
-    <View>
-      <Text>App.js</Text>
-    </View>
+    <Router>
+      <Scene key='root'>
+        <Scene key='home' component={Home} title='Home'/>
+        <Scene key='chat' component={Chat} title='Chat' />
+
+      </Scene>
+    </Router>
   );
 }
